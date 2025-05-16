9947a40a1063dbd55820df9b38756a94873e71fa
LuanDias19 <lumardias07@gmail.com>
2025-05-16 11:49:19 -0300

    Finalizando tipos e variaveis

---
+++ javaAnatomiaDeClasses/src/edu/luan/tiposVariaveis/Aula4.java
@@ -0,0 +1,18 @@
+package edu.luan.tiposVariaveis;
+
+public class Aula4 {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println("Número: " + numero);
+    }
+}
