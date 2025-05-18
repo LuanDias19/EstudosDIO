6da0205694ff34633adf4e0ed2e0fcf0b105e3c2
LuanDias19 <lumardias07@gmail.com>
2025-05-18 15:43:31 -0300

    Subindo arquivos de Controle de Fluxo

--- controleDeFluxo/src/PlanoOperadora.java
+++ controleDeFluxo/src/PlanoOperadora.java
@@ -1,18 +1,28 @@
-// Modo condicional convencional
-public class PlanoOperadora {
-    public static void main(String[] args) {
-        String plano = "M"; // M / T
-
-        if (plano == "B") {
-            System.out.println("100 minutos de ligação");
-        } else if (plano == "M") {
-            System.out.println("100 minutos de ligação");
-            System.out.println("Whats e Intagram grátis");
-        } else if (plano == "T") {
-            System.out.println("100 minutos de ligação");
-            System.out.println("Whats e Intagram grátis");
-            System.out.println("5Gb Youtube");
-        }
-
-    }
-}
+/*
+ * public class PlanoOperadora {
+ * public static void main(String[] args) {
+ * String plano = "M"; // M / T
+ * 
+ * if (plano == "B") {
+ * System.out.println("100 minutos de ligação");
+ * } else if (plano == "M") {
+ * System.out.println("100 minutos de ligação");
+ * System.out.println("Whats e Intagram grátis");
+ * } else if (plano == "T") {
+ * System.out.println("100 minutos de ligação");
+ * System.out.println("Whats e Intagram grátis");
+ * System.out.println("5Gb Youtube");
+ * }
+ * 
+ * }
+ * }
+ * 
+ * public static void main(String[] args) {
+ * double alturaJoao = 1.46;
+ * double alturaMarcos = 1.37;
+ * if (alturaMarcos < alturaJoao)
+ * System.out.println("João é mais alto que Marcos");
+ * else
+ * System.out.println("Marcos é mais baixo que João");
+ * }
+ */
