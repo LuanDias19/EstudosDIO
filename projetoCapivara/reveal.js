59a5abdd5406aea975795894d270d5e6cff8bc6d
LuanDias19 <lumardias07@gmail.com>
2025-07-28 19:21:32 -0300

    Subindo o projeto 'Resistencia Capivariana'

---
+++ projetoCapivara/reveal.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal(".conteudo-principal", {
+  origin: "top",
+  distance: "50px",
+  duration: 1000,
+  delay: 200,
+  easing: "ease-out",
+  reset: false,
+});
+
+ScrollReveal().reveal(".conteudo-secundario", {
+  origin: "top",
+  distance: "60px",
+  duration: 1000,
+  delay: 300,
+  easing: "ease-out",
+  reset: false,
+  viewFactor: 0.1,
+  opacity: 1,
+});
